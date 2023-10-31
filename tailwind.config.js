d5230582aaa41c1823bd57096e25839414797aca
ArkanaStudio <dwika820@gmail.com>
2023-10-31 21:35:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        'dark' : '#0c4a6e',
+        'dark-paragraph' : '#64748b',
+        'second-dark' : '#0f172a',
+        'light' : '#fafafa',
+        'light-paragraph' : '#cbd5e1',
+        'primary' : '#0284c7',
+        'secondary' : '#0ea5e9',
+      },
+
+      fontFamily: {
+        'poppins' : 'poppins',
+        'roboto' : 'roboto'
+      }
+    },
+  },
+  plugins: [],
+}
+
